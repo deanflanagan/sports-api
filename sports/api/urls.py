d867cb14200edd25c8fcb3e2a0f6edd08bc868e2
Dean <deanflanagan@gmail.com>
2022-05-14 18:52:53 -0300

    api: new urls to map for button plot

--- sports/api/urls.py
+++ sports/api/urls.py
@@ -20,6 +20,13 @@ urlpatterns = [
     path('game/<int:game_id>/placeholder', views.get_game_placeholder, name='get_game_placeholder'),
     path('game/<int:game_id>/league', views.get_game_league, name='get_game_league'),
 
+    path('game/<int:game_id>/team-plot-venues', views.team_plot_venues, name='team_plot_venues'),
+    path('game/<int:game_id>/team-plot-fave-dog', views.team_plot_fave_dog, name='team_plot_fave_dog'),
+    path('game/<int:game_id>/team-plot-home-fave-dog', views.team_plot_home_fave_dog, name='team_plot_home_fave_dog'),
+    path('game/<int:game_id>/opposition-plot-venues', views.opposition_plot_venues, name='opposition_plot_venues'),
+    path('game/<int:game_id>/opposition-plot-fave-dog', views.opposition_plot_fave_dog, name='opposition_plot_fave_dog'),
+    path('game/<int:game_id>/opposition-plot-away-fave-dog', views.opposition_plot_away_fave_dog, name='opposition_plot_away_fave_dog'),
+
     path('bets/',views.get_bets, name='get_bets'),
     path('strategies/',views.get_strategies, name='get_strategies'),
     path('preferences/',views.get_preferences, name='get_preferences'),
